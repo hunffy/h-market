983eb6b8bc9f6371f34d131beb920891d5103076
ksh <sehun910124@naver.com>
2024-01-11 13:30:17 +0900

    상품 업로드 폼  antd 활용하여 작업

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,5 +1,76 @@
+import { Button, Divider, Form, Input, InputNumber } from "antd";
+import "./index.css";
 function UploadPage() {
-  return <h1>업로드화면</h1>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <div id="upload-container">
+      <Form name="상품 업로드" onFinish={onSubmit}>
+        <Form.Item
+          name="upload"
+          label={<div className="upload-label">상품 이미지</div>}
+        >
+          <div id="upload-img-placeholder">
+            <img src="/images/icons/camera.png" alt="이미지업로드" />
+            <span>이미지를 업로드해주세요.</span>
+          </div>
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="seller"
+          //rules required:true => 입력을 꼭 해야된다. 안하면 message에 설정한 글이 나타난다.
+          rules={[{ required: true, message: "판매자 이름을 입력해주세요" }]}
+          label={<div className="upload-label">판매자 명</div>}
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="이름을 입력해주세요"
+          />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="name"
+          label={<div className="upload-label">상품이름</div>}
+          rules={[{ required: true, message: "상품 이름을 입력해주세요" }]}
+        >
+          <Input
+            className="upload-name"
+            size="large"
+            placeholder="상품 이름을 입력해주세요"
+          />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="price"
+          label={<div className="upload-label">상품가격</div>}
+          rules={[{ required: true, message: "상품 가격을 입력해주세요" }]}
+        >
+          <InputNumber className="upload-price" size="large" defaultValue={0} />
+        </Form.Item>
+        <Divider />
+        <Form.Item
+          name="description"
+          label={<div className="upload-label">상품소개</div>}
+          rules={[{ required: true, message: "상품 소개를 입력해주세요" }]}
+        >
+          <Input.TextArea
+            size="large"
+            id="product-description"
+            showCount
+            maxLength={300}
+            placeholder="상품 설명을 적어주세요"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button id="submit-button" size="large" htmlType="submit">
+            문제 등록하기
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
 
 export default UploadPage;
