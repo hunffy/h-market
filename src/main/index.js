6538217b0533fa0c982f89a52d94f038ad9c0dc0
ksh <sehun910124@naver.com>
2024-01-13 16:00:35 +0900

    antd Carousel 사용하여 슬라이드 이미지구현

--- src/main/index.js
+++ src/main/index.js
@@ -8,6 +8,9 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import locale from "dayjs/locale/ko";
 
 import { API_BASE_URL } from "../config/constants";
+import { Carousel } from "antd";
+
+import { useNavigate } from "react-router-dom";
 /* dayjs, relativeTime import 하고 
 
 dayjs.extend를 통해 relativeTime넣어주어 확장시키면
@@ -22,6 +25,8 @@ dayjs.locale("ko");
 
 function MainPage() {
   const [products, setProducts] = useState([]);
+  const [banners, setBanners] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
@@ -33,13 +38,34 @@ function MainPage() {
       .catch(function (error) {
         console.log("에러발생", error);
       });
+
+    axios
+      .get(`${API_BASE_URL}/banners`)
+      .then((result) => {
+        const banners = result.data.banners;
+        setBanners(banners);
+      })
+      .catch((error) => {
+        console.log("error : ", error);
+      });
   }, []);
 
   return (
     <div className="MainPage">
-      <div id="banner">
-        <img src="images/banners/banner1.png" alt="bannerImg" />
-      </div>
+      <Carousel autoplay autoplaySpeed={3000}>
+        {banners.map((it, index) => {
+          return (
+            <div
+              id="banner"
+              onClick={() => {
+                navigate(`${it.href}`);
+              }}
+            >
+              <img src={`${API_BASE_URL}/${it.imageUrl}`} alt="bannerImg" />
+            </div>
+          );
+        })}
+      </Carousel>
       <h1 id="product-headline">판매되는 상품들</h1>
       <div id="product-list">
         {products.map((it, index) => (
