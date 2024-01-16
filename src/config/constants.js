0185242252d91128061e8e736ba3e398545f5953
ksh <sehun910124@naver.com>
2024-01-16 15:02:13 +0900

    api url 주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,4 @@
-export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://h-market-server.fly.dev"
+    : "http://localhost:8080";
